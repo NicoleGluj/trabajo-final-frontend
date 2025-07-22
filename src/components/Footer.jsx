d5bb027eb884c4ccf44384a566e0c05c3e31939b
NicoleGluj <nicolegluj@gmail.com>
2025-07-22 16:12:52 -0300

    add: layout terminado

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>sitio desarrollado por<a href="https://www.linkedin.com/in/nicole-gluj-640805210/" target="blank"> Nicole Gluj</a></p>
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+const Header = () => {
+  return (
+    <header>
+      <img src="" alt="imagen de logo" />
+      <nav>
+        <ul>
+          <li><a href="/">INICIO</a></li>
+          <li><a href="/dashboard">DASHBOARD</a></li>
+          <li><a href="/login">LOGIN</a></li>
+          <li><a href="/register">REGISTRO</a></li>
+          <button>CERRAR SESION</button>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header;

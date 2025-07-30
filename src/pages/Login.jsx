5d873a783c424f523f08c65008cd6f3ccbfed7f9
NicoleGluj <nicolegluj@gmail.com>
2025-07-29 22:02:31 -0300

    add: validacion login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,20 +1,57 @@
+import { useState } from "react";
 import CardAuth from "../components/CardAuth";
 import Layout from "../components/Layout"
 import '../styles/pages/login.css'
 
 const Login = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setError("")
+
+    if (!email || !password) {
+      setError("Debe completar todos los campos")
+      return
+    }
+
+    const User = {
+      email: email,
+      password: password
+    }
+
+    setEmail("")
+    setPassword("")
+  }
+
   return (
     <Layout>
       <CardAuth>
         <div className='card-auth-content'>
           <h3>INICIA SESION</h3>
-          <div className="card-inputs">
-            <label>Correo Electronico</label>
-            <input type="email" />
-            <label>Contraseña</label>
-            <input type="password" />
-          </div>
-          <button className="card-button">INGRESAR</button>
+          <form className="card-form" onSubmit={handleSubmit}>
+            <div className="card-input">
+              <label>Correo Electronico</label>
+              <input
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email} />
+            </div>
+            <div className="card-input">
+              <label>Contraseña</label>
+              <input
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password} />
+            </div>
+            <button className="card-button">INGRESAR</button>
+          </form>
+
+          {
+            error && <p>{error}</p>
+          }
         </div>
       </CardAuth>
     </Layout>

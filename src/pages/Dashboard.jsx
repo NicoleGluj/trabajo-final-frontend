e03cfed38c070880165e9a73c9243145d7487f97
NicoleGluj <nicolegluj@gmail.com>
2025-07-30 23:59:49 -0300

    add: funcion POST dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,26 +1,90 @@
+import { useState } from "react";
 import Layout from "../components/Layout";
 
 const Dashboard = () => {
+
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState("")
+  const [description, setDescription] = useState("")
+  const [product, setProduct] = useState(null)
+  const [error, setError] = useState(null)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setError(null)
+
+    if (!name || !price || !description) {
+      setError("Debes completar todos los campos")
+      return
+    }
+
+    const newProduct = {
+      id: crypto.randomUUID(),
+      title: name,
+      price: price,
+      description: description,
+      category: "",
+      image: ""
+    }
+
+    const response = await fetch('https://fakestoreapi.com/products', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newProduct)
+    })
+
+    setProduct(newProduct)
+
+    setName("")
+    setPrice("")
+    setDescription("")
+  }
+
+
   return (
     <Layout>
       <div>
         <h2>cargar nuevo producto</h2>
       </div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label>Nombre del producto</label>
-          <input type="text" name="nombre" />
+          <input
+            type="text"
+            name="nombre"
+            onChange={(e) => setName(e.target.value)}
+            value={name} />
         </div>
         <div>
           <label>Precio del producto</label>
-          <input type="number" name="precio" />
+          <input
+            type="number"
+            name="precio"
+            onChange={(e) => setPrice(e.target.value)}
+            value={price} />
         </div>
         <div>
           <label>Descripcion del producto</label>
-          <input type="text" name="descripcion" />
+          <input
+            type="text"
+            name="descripcion"
+            onChange={(e) => setDescription(e.target.value)}
+            value={description} />
         </div>
         <button>GUARDAR PRODUCTO</button>
       </form>
+      {
+        error && <div>{error}</div>
+      }
+      {
+        product && <div>
+          <h3>{product.title}</h3>
+          <h3>{product.price}</h3>
+          <h3>{product.description}</h3>
+        </div>
+      }
     </Layout>
   )
 }

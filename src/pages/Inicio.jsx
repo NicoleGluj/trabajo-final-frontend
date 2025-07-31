cfc544f846bcd31db4071589eebdc4f9e46f11e6
NicoleGluj <nicolegluj@gmail.com>
2025-07-31 01:47:13 -0300

    add: update product funcion

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -4,6 +4,13 @@ import Layout from "../components/Layout"
 const Inicio = () => {
   const [products, setProducts] = useState([])
   const [user, setUser] = useState(true)
+  const [productToEdit, setProductToEdit] = useState(null)
+  const [showPopUp, setShowPopUp] = useState(null)
+  const [titleEdit, setTitleEdit] = useState("")
+  const [priceEdit, setPriceEdit] = useState("")
+  const [descriptionEdit, setDescriptionEdit] = useState("")
+  const [categoryEdit, setCategoryEdit] = useState("")
+  const [imageEdit, setImageEdit] = useState("")
 
   const fetchingProducts = async () => {
     const response = await fetch('https://fakestoreapi.com/products', { method: "GET" })
@@ -23,8 +30,85 @@ const Inicio = () => {
     setProducts(prevProducts => prevProducts.filter((product) => product.id != id))
   }
 
+  const handleOpenEdit = (product) => {
+    setShowPopUp(true)
+    setProductToEdit(product)
+    setTitleEdit(product.title)
+    setPriceEdit(product.price)
+    setDescriptionEdit(product.description)
+    setCategoryEdit(product.category)
+    setImageEdit(product.image)
+  }
+
+  const handleUpdate = async (e) => {
+    e.preventDefault()
+
+    const updatedProduct = {
+      id: productToEdit.id,
+      title: titleEdit,
+      price: Number(priceEdit),
+      description: descriptionEdit,
+      category: categoryEdit,
+      image: imageEdit
+    }
+
+    const response = await fetch(`https://fakestoreapi.com/products/${productToEdit.id}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "Application/json"
+        },
+        body: JSON.stringify(updatedProduct)
+      })
+
+    if (response.ok) {
+      const data = await response.json()
+      setProducts(prevProduct =>
+        prevProduct.map((product) =>
+          product.id === productToEdit.id
+            ? data
+            : product)
+      )
+    }
+  }
+
+
+
   return (
     <Layout>
+      {
+        showPopUp && <section>
+          <h2>Editar Producto</h2>
+          <button onClick={() => setShowPopUp(null)}>cerrar</button>
+          <form onSubmit={handleUpdate}>
+            <input
+              type="text"
+              placeholder="Ingrese el titulo"
+              value={titleEdit}
+              onChange={(e) => setTitleEdit(e.target.value)} />
+            <input
+              type="number"
+              placeholder="Ingrese el precio"
+              value={priceEdit}
+              onChange={(e) => setPriceEdit(e.target.value)} />
+            <textarea
+              placeholder="Ingrese la descripcion"
+              value={descriptionEdit}
+              onChange={(e) => setDescriptionEdit(e.target.value)} ></textarea>
+            <input
+              type="text"
+              placeholder="Ingrese la categoria"
+              value={categoryEdit}
+              onChange={(e) => setCategoryEdit(e.target.value)} />
+            <input
+              type="text"
+              placeholder="Ingrese la URL de la imagen"
+              value={imageEdit}
+              onChange={(e) => setImageEdit(e.target.value)} />
+            <button>ACTUALIZAR</button>
+          </form>
+        </section>
+      }
       {
         products.map((product) =>
           <div key={product.id}>
@@ -35,7 +119,7 @@ const Inicio = () => {
             <p><strong>{product.category}</strong></p>
             {
               user && <div>
-                <button>ACTUALIZAR</button>
+                <button onClick={() => handleOpenEdit(product)}>ACTUALIZAR</button>
                 <button onClick={() => handleDelete(product.id)}>BORRAR</button>
               </div>
             }

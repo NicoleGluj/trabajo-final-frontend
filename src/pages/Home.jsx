06bb7f5b57adcdfacb5f0bcc719b8766fd9562ed
NicoleGluj <nicolegluj@gmail.com>
2025-08-01 17:33:20 -0300

    fixed: home style y services

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Layout from "../components/Layout"
 import '../styles/pages/home.css'
 
@@ -7,7 +8,7 @@ const Home = () => {
       <section id="content-inicio">
         <div className="content-inicio-text">
           <h1>THE PRODUCT</h1>
-          <h2>CONECTA CON UNA NUEVA FORMA DE VENDER .</h2>
+          <h2>CONECTA CON UNA NUEVA FORMA DE COMPRAR.</h2>
         </div>
       </section>
 
@@ -15,8 +16,8 @@ const Home = () => {
         <img src="src/assets/cerrar-sonriente-mujer-sosteniendo-smartphone.jpg" alt="" />
         <div className="about-title">
           <h2>¿QUIENES SOMOS?</h2>
-          <p>Nos dedicamos a brindar un espacio digital diseñado especialmente para que cualquier negocio, marca o emprendedor pueda mostrar y compartir sus catálogos de productos de manera fácil y organizada. <br /> Nuestro objetivo es conectar a vendedores y compradores en un entorno confiable, moderno y accesible, donde cada publicación tenga la visibilidad que merece. <br /> Creemos en impulsar a los emprendedores y pequeñas empresas, ofreciéndoles una herramienta práctica para potenciar sus ventas, llegar a más personas y hacer crecer sus proyectos sin complicaciones.</p>
-          <button className="about-button">+ QUIERO SER PARTE</button>
+          <p>Somos una tienda online pensada para reunir en un solo lugar una amplia variedad de productos de diferentes categorías. <br /> Creemos en la comodidad, la calidad y la atención personalizada. Por eso, trabajamos todos los días para ofrecerte una experiencia de compra fácil, segura y con opciones para todos los gustos y necesidades. <br /> The Product es un espacio pensado para vos, para acompañarte y hacer que encontrar lo que necesitás sea fácil, rápido y agradable. Porque creemos que cada producto que elegís puede sumar a tu día a día, y queremos ser parte de esa eleccion. </p>
+          <Link to="/inicio" className="about-button">+ VER CATALOGO</Link>
         </div>
       </section>
 
@@ -27,23 +28,21 @@ const Home = () => {
         <div className="services-content">
           <div className="service-content">
             <i className="bi bi-bag-plus"></i>
-            <h3>Publicación rápida de productos</h3>
-            <p>Subí tus productos en minutos: cargá fotos, precios y descripciones de forma simple e intuitiva.</p>
+            <h3>Compra fácil y rápida</h3>
+            <p>Hacé tu pedido en pocos pasos y disfrutá de una experiencia sencilla y sin complicaciones.</p>
           </div>
           <div className="service-content">
-            <i className="bi bi-grid-3x3-gap"></i>
-            <h3> Catálogos digitales organizados y personalizables</h3>
-            <p>Organizá tus productos en catálogos digitales claros y atractivos, fáciles de actualizar y listos para que tus clientes los vean en cualquier momento.</p>
+            <i class="bi bi-box-seam"></i>
+            <h3>Envios a todo el país</h3>
+            <p>Enviamos a todo el país para que tus compras lleguen rápido y seguro hasta la puerta de tu casa.</p>
           </div>
           <div className="service-content">
-            <i className="bi bi-graph-up-arrow"></i>
-            <h3>Mayor alcance para tus productos</h3>
-            <p>Mostrá tus productos a más personas y hacé que tu catálogo llegue a potenciales clientes interesados.</p>
+            <i class="bi bi-wallet2"></i>            <h3>Pago 100% seguro</h3>
+            <p>Tus datos y transacciones están protegidos con tecnología avanzada para garantizar tu tranquilidad.</p>
           </div>
           <div className="service-content">
-            <i className="bi bi-share"></i>
-            <h3>Explorá catálogos organizados</h3>
-            <p>Descubrí productos únicos y ordenados por categorías para que sea fácil encontrar lo que buscás.</p>
+            <i class="bi bi-people-fill"></i>            <h3>Soporte</h3>
+            <p>Contamos con un equipo de atención disponible para responder tus consultas y ayudarte en todo momento.</p>
           </div>
         </div>
       </section>

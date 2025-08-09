3699cd1656171f3b3a312026e1f2c0f066c1929f
NicoleGluj <nicolegluj@gmail.com>
2025-08-09 00:03:19 -0300

    fixed: handle logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,4 @@
-import { Link } from "react-router-dom";
-// import '../styles/components/header.css'
+import { Link, useNavigate } from "react-router-dom";
 import '../styles/style.css';
 import { useState } from "react";
 import { useAuth } from "../context/UserContext";
@@ -8,21 +7,24 @@ const Header = () => {
   const [isOpen, setIsOpen] = useState(false)
   const { user, logout } = useAuth()
 
+  const navigate = useNavigate()
+
   const handleOpen = () => {
     setIsOpen(!isOpen)
   }
 
   const handleLogout = () => {
     logout()
+    navigate("/")
   }
 
   return (
-    <header className="m-[15px] bg-[#ececec] p-2 rounded-[15px]">
+    <header className="m-4 bg-[#ececec] p-2 rounded-2xl">
       <nav className="min-[880px]:flex min-[880px]:flex-row min-[880px]:items-center min-[880px]:justify-between ">
         <section className="flex justify-between items-center">
           <div className="flex items-center justify-center">
-            <img src="src\assets\icono-favicon.png" alt="logo TheProduct" className="w-7 h-7 mr-2" />
-            <h2 className="font-semibold text-[18px]">THE PRODUCT</h2>
+            <img src="src\assets\icono-favicon.png" alt="logo TheProduct" className="size-7 mr-2" />
+            <h2 className="font-semibold text-xl">THE PRODUCT</h2>
           </div>
           <div className="min-[880px]:hidden">
             <i
@@ -32,18 +34,18 @@ const Header = () => {
         </section>
 
         <section className={`min-[880px]:flex min-[880px]:flex-row ${isOpen ? 'flex' : 'hidden'} min-[880px]:items-center`}>
-          <ul className="w-full text-gray-700 mt-4 flex flex-col items-center justify-center min-[880px]:flex min-[880px]:flex-row mb-5 min-[880px]:mb-0 min-[880px]:gap-4 min-[880px]:justify-center min-[880px]:mt-0" >
-            <li className="hover:text-[18px] mb-2 min-[880px]:mb-0"><Link to="/">INICIO</Link></li>
+          <ul className="w-full text-gray-700 mt-4 flex flex-col min-[880px]:flex-row items-center justify-center mb-5 min-[880px]:mb-0 min-[880px]:gap-4 min-[880px]:mt-0" >
+            <li className="mb-2 min-[880px]:mb-0"><Link to="/">INICIO</Link></li>
             {
               user &&
-              <li className="hover:text-[18px] mb-2 min-[880px]:mb-0"><Link to="/dashboard">DASHBOARD</Link></li>
+              <li className="mb-2 min-[880px]:mb-0"><Link to="/dashboard">DASHBOARD</Link></li>
             }
             {
               !user && <>
-                <li className="hover:text-[17px] mb-2 min-[880px]:mb-0"><a href="#content-about">CONOCENOS</a></li>
-                <li className="hover:text-[17px] mb-2 min-[880px]:mb-0"><a href="#content-services">SERVICIOS </a></li>
-                <li className="hover:text-[17px] mb-2 min-[880px]:mb-0"><a href="#content-services"> PROYECTO </a></li>
-                <li className="hover:text-[17px] mb-2 min-[880px]:mb-0"><a href="#content-footer">CONTACTO </a></li>
+                <li className="mb-2 min-[880px]:mb-0"><a href="#content-about">CONOCENOS</a></li>
+                <li className="mb-2 min-[880px]:mb-0"><a href="#content-services">SERVICIOS </a></li>
+                <li className="mb-2 min-[880px]:mb-0"><a href="#content-services"> PROYECTO </a></li>
+                <li className="mb-2 min-[880px]:mb-0"><a href="#content-footer">CONTACTO </a></li>
               </>
             }
           </ul>
@@ -53,16 +55,18 @@ const Header = () => {
           <div className="w-full items-center justify-center flex flex-row gap-3 mt-2 min-[880px]:mt-0">
             {
               user &&
-              <button onClick={handleLogout}>
+              <button
+                className="p-2 bg-[#ffb500] px-4 py-1.5 rounded-full font-medium text-base text-white tracking-[1px]"
+                onClick={handleLogout}>
                 <Link to="/home" >CERRAR SESION</Link>
               </button>
             }
             {
               !user && <>
-                <button className="bg-[#ffb500] px-4 py-1.5 rounded-[15px] font-[600] text-white tracking-[1px]">
+                <button className="p-2 bg-[#ffb500] px-4 py-1.5 rounded-full font-medium text-base text-white tracking-[1px]">
                   <Link to="/login">INGRESAR</Link>
                 </button>
-                <button className="border-3 border-[#FFB500] text-[#eba800] bg-white font-[700] px-4 py-1 rounded-[15px] tracking-[1px]">
+                <button className="p-2 bg-white/70 text-[#ffb500] px-4 py-1.5 rounded-full font-medium text-base tracking-[1px] border-2">
                   <Link to="/register">REGISTRAR</Link>
                 </button>
               </>

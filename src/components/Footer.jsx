55f606144e8b874388df69ddb05c591159806015
NicoleGluj <nicolegluj@gmail.com>
2025-08-12 00:38:54 -0300

    fixed: style footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,35 @@
-import '../styles/components/footer.css'
 
 const Footer = () => {
   return (
-    // <footer id='content-footer'>
-    //   <div id='text-footer'>
-    //     <div className='contact-content'>
-    //       <h3>CONTACTO</h3>
-    //       <p>TheProductArea@gmail.com</p>
-    //       <p>+54 9 11 3456 7898</p>
-    //       <p>Buenos Aires, Argentina</p>
-    //     </div>
-    //     <div className='coment-content'>
-    //       <h3>DEJANOS TU COMENTARIO</h3>
-    //       <label>Correo Electronico</label>
-    //       <input type="email" />
-    //       <label>Mensaje</label>
-    //       <textarea></textarea>
-    //     </div>
-    //   </div>
-    //   <p className='text-desarrollado'>Sitio desarrollado por<a href="https://www.linkedin.com/in/nicole-gluj-640805210/" target="blank"> Nicole Gluj</a></p>
-    // </footer>
-    <>
-    </>
+    <footer
+      id='content-footer'
+      className="bg-[#ffb500] mt-10">
+      <div className="flex justify-around text-white p-8">
+        <div className="flex flex-col gap-2">
+          <h3 className="font-bold text-xl mb-2"
+          >CONTACTO</h3>
+          <p>TheProductArea@gmail.com</p>
+          <p>+54 9 11 3456 7898</p>
+          <p>Buenos Aires, Argentina</p>
+        </div>
+        <div className="flex flex-col gap-2">
+          <h3 className="font-bold text-xl mb-2"
+          >DEJANOS TU COMENTARIO</h3>
+          <label>Correo Electronico</label>
+          <input
+            type="email"
+            className="bg-white p-1 rounded-2xl" />
+          <label>Mensaje</label>
+          <textarea className="bg-white rounded-2xl"></textarea>
+          <button className="bg-white px-7 py-1 text-gray-600 rounded-2xl w-min font-semibold text-sm hover:scale-105 hover:text-black transition-all duration-200  "
+          >ENVIAR</button>
+        </div>
+      </div>
+      <div className="text-center font-normal text-black border-t-1 mx-5">
+        <p className="mt-3"
+        >Sitio desarrollado por<a href="https://www.linkedin.com/in/nicole-gluj-640805210/" target="blank"> Nicole Gluj</a></p>
+      </div>
+    </footer>
   )
 }
 
